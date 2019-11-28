8e00e996714511632feef3eb942e77896ad91c27
obafemi oderanti <thevetdoctor.gmail.com>
2019-11-28 11:05:03 +0100

    chore(setup):setup development envrionment
    setup file structure redux store reducers and actions
    
    [Finishes @29825626]

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -7,27 +7,32 @@ class App extends Component {
     super(props);
   
    this.state = {
-    firstname: '',
-    lastname: '',
-    birthday: '', 
-    age: '',
-    hobby: '',
+    firstName: '',
+    lastName: '',
+    email: '', 
+    password: '',
+    gender: '',
+    jobRole: '',
+    department: '',
+    address: '',
   }
 
    this.handleChange = this.handleChange.bind(this);
-  //  this.handleSubmit = this.handleSubmit.bind(this);
+   this.handleSubmit = this.handleSubmit.bind(this);
 }
 
 
+handleSubmit (e) {
+  e.preventDefault();
+  let inputClass = e.target.children[9].classList;
+  console.log(inputClass.toggle('inactive'));
+}
 
 
 handleChange({target}) {
   let name = target.name;
   let val = target.value;
-  let keyArray = ['firstname', 'lastname', 'birthday', 'age', 'hobby'];
-  if (name === undefined) {
-  this.setState(prev => ({birthday: ''}));
-  }
+  let keyArray = ['firstName', 'lastName', 'email', 'password', 'gender', 'jobRole', 'department', 'address'];
   if (keyArray.indexOf(name) >= 0) {
   this.setState(prev => ({[name]: ''}));
   }
@@ -50,10 +55,7 @@ handleChange({target}) {
     }
   }
   
-if (name === '') {
-  this.setState(prev => ({birthday: val}));
-  return;
-}
+
   this.setState(prev => ({[name]: val}));
 }
 
@@ -62,21 +64,22 @@ render() {
 
   return (
     <div className="App">
-        <a href='/'>Back</a>
-        <a href='/api/v1/docs'>Documentation</a>
+        {/* <a href='/'>Back</a> */}
+        {/* <a href='/api/v1/docs'>Documentation</a> */}
         <h3>New User Account</h3>
         Please fill in user details
 
-        <form className='form-selector'>
-          <input type='text' name='firstName' placeholder='Firstname' />
-          <input type='text' name='lastName' placeholder='Lastname' />
-          <input type='text' name='email' placeholder='Email' />
-          <input type='text' name='password' placeholder='Password' />
-          <input type='text' name='gender' placeholder='Gender' />
-          <input type='text' name='jobRole' placeholder='Job Role' />
-          <input type='text' name='department' placeholder='Department' />
-          <input type='text' name='address' placeholder='Address' />
-          <input type='submit' name='Create' value='Create' />
+        <form className='form-selector' onSubmit={this.handleSubmit}>
+          <input type='text' name='firstName' placeholder='Enter Firstname' onChange={this.handleChange} />
+          <input type='text' name='lastName' placeholder='Enter Lastname' onChange={this.handleChange} />
+          <input type='text' name='email' placeholder='Put in Email' onChange={this.handleChange} />
+          <input type='password' name='password1' placeholder='Enter Password' onChange={this.handleChange} />
+          <input type='password' name='password2' placeholder='Re-enter Password' onChange={this.handleChange} />
+          <input type='text' name='gender' placeholder='Enter Gender' onChange={this.handleChange} />
+          <input type='text' name='jobRole' placeholder="What's the Job Role" onChange={this.handleChange} />
+          <input type='text' name='department' placeholder='Which Department' onChange={this.handleChange} />
+          <input type='text' name='address' placeholder='Enter Address' onChange={this.handleChange} />
+          <input className='submit-inactive' type='submit' name='create-account' value='Create account' onClick={() => this.props.onClick(this.state)} />
         </form>
     </div>
     );
