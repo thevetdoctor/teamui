a6ba8299ee375de6e8126d070656255f06c1b7de
obafemi oderanti <thevetdoctor.gmail.com>
2019-12-02 03:39:18 +0100

    Updates

---
+++ precache-manifest.2baefac7bb2985b1ea96f7ec5aa29129.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4faa387bdae4750c0c931e74e76e0470",
+    "url": "/teamui/index.html"
+  },
+  {
+    "revision": "8bc41b587d62353c794d",
+    "url": "/teamui/static/css/main.78a167ff.chunk.css"
+  },
+  {
+    "revision": "a333ec9a82a0b3044ae4",
+    "url": "/teamui/static/js/2.53de8a2e.chunk.js"
+  },
+  {
+    "revision": "8bc41b587d62353c794d",
+    "url": "/teamui/static/js/main.6b49f2a8.chunk.js"
+  },
+  {
+    "revision": "e5fcf322b27c3bcc2c70",
+    "url": "/teamui/static/js/runtime-main.17d6ce57.js"
+  }
+]);
